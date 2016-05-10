9fbecee4fc56e634340c9607f1b63f9b63a18e92
Team-MC2 <teammc2monash@gmail.com>
2016-05-10 23:43:23 +1000

    basic routing

--- com.jpagliuca.recipi/src/app.js
+++ com.jpagliuca.recipi/src/app.js
@@ -3,4 +3,17 @@
 var app = angular.module('recipi', [
     'services',
     'controllers',
-]);
+    'ngRoute'
+])
+    .config(function($routeProvider) {
+        $routeProvider
+            // home page
+            .when('/', {
+                templateUrl: 'home.html'
+            })
+
+            .when('/recipe', {
+                templateUrl: 'recipe.html',
+                controller: 'stepListController'
+            })
+    });
--- com.jpagliuca.recipi/src/test/karma.conf.js
+++ com.jpagliuca.recipi/src/test/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
     files: [
       '../assets/js/angular.min.js',
       '../assets/js/angular-mocks.js',
+      '../assets/js/angular-route.min.js',
       '../*.js',
       '*.spec.js'
     ],
