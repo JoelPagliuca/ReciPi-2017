3f78d62013eec1456735c38f0c48fbeab23a7da3
Team-MC2 <teammc2monash@gmail.com>
2016-05-11 19:38:32 +1000

    added unit test for the getIngredientList function. Minor ui changes to recipe.html

--- com.jpagliuca.recipi/src/controllers.js
+++ com.jpagliuca.recipi/src/controllers.js
@@ -3,34 +3,61 @@
 /** controls the list of recipes */
 angular.module('controllers', [])
 
+    /**
+     * gets a list of recipes
+     */
     .controller('recipeListController', function($scope, recipeService) {
         var data = recipeService.getRecipes();
         $scope.recipes = data;
         $scope.recipesCount = data.length;
     })
 
+    /**
+     * gets the recipe and steps
+     * calculates the ingredients list of the recipe
+     */
     .controller('stepListController', function($scope, $routeParams, recipeService) {
+        // get the recipe id from the GET param
         $scope.recipe_id = $routeParams.id;
-        var data = recipeService.getSteps($scope.recipe_id);
-        $scope.recipe = recipeService.getRecipe($scope.recipe_id);
-        $scope.steps = data;
 
+        // get the data
+        $scope.recipe = recipeService.getRecipe($scope.recipe_id);
+        $scope.steps = recipeService.getSteps($scope.recipe_id);
+
+        var ingredientList = getIngredientList($scope.steps);
         // ingredients calculation
-        $scope.ingredients = [];
+        $scope.ingredients = ingredientList.ingredients;
         // maps for amounts and quantities
-        $scope.amount = {};
-        $scope.unit = {};
-        // get all the unique ingredients
-        for (var i = 0, len = data.length; i<len; i++) {
-            var ing = $scope.steps[i].ingredient;
-            var idx = $scope.ingredients.indexOf(ing);
-            if (idx === -1) {
-                $scope.ingredients.push(ing);
-                $scope.amount[ing] = $scope.steps[i].amount;
-                $scope.unit[ing] = $scope.steps[i].unit;
-            } else {
-                $scope.amount[ing] += $scope.steps[i].amount;
-            }
-        }
-        console.log($scope.amount);
+        $scope.amount = ingredientList.amount;
+        $scope.unit = ingredientList.unit;
     });
+
+/**
+ * Calculates the ingredients list for a recipe given its steps
+ * @param steps: Array
+ * @returns {{ingredients: Array, amount: {}, unit: {}}}
+ */
+function getIngredientList (steps) {
+    // ingredients calculation
+    var ingredients = [];
+    // maps for amounts and quantities
+    var amount = {}, // {ingredient -> amount}
+        unit = {};   // {ingredient -> unit}
+    // get all the unique ingredients
+    for (var i = 0, len = steps.length; i<len; i++) {
+        var ing = steps[i].ingredient;
+        var idx = ingredients.indexOf(ing);
+        if (idx === -1) {
+            ingredients.push(ing);
+            amount[ing] = steps[i].amount;
+            unit[ing] = steps[i].unit;
+        } else {
+            amount[ing] += steps[i].amount;
+        }
+    }
+    return {
+        ingredients: ingredients,
+        amount: amount,
+        unit: unit
+    };
+}
--- com.jpagliuca.recipi/src/test/controllers.spec.js
+++ com.jpagliuca.recipi/src/test/controllers.spec.js
@@ -23,4 +23,34 @@ describe('controllers', function() {
         });
     });
 
+    describe('getIngredientList', function() {
+
+        var test_steps = [
+            { ingredient: 'a', unit:'x', amount:1 },
+            { ingredient: 'b', unit:'y', amount:2 },
+            { ingredient: 'c', unit:'x', amount:3 }
+        ];
+
+        it('should do nothing for empty list', function() {
+            var data = getIngredientList([]);
+            expect(data.ingredients).toEqual([]);
+            expect(data.amount).toEqual({});
+            expect(data.unit).toEqual({});
+        });
+
+        it('should get a list of ingredients', function() {
+            var data = getIngredientList(test_steps);
+            expect(data.ingredients).toEqual(['a', 'b', 'c']);
+            expect(Object.keys(data.amount)).toEqual(['a', 'b', 'c']);
+            expect(Object.keys(data.unit)).toEqual(['a', 'b', 'c']);
+        });
+
+        it('should calculate the amount correctly', function() {
+            test_steps[2].ingredient = 'a';
+            var data = getIngredientList(test_steps);
+            expect(data.amount['a']).toBe(4);
+        });
+
+    });
+
 });
