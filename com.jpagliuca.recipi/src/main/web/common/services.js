647c1181ce8fcdbc16abb5cd8bb4ee87a53a47e5
Team-MC2 <teammc2monash@gmail.com>
2016-05-14 19:42:02 +1000

    fixed the steps so that they have proper ingredients, also the step list and ingredient list are now displaying them correctly

--- com.jpagliuca.recipi/src/main/web/common/services.js
+++ com.jpagliuca.recipi/src/main/web/common/services.js
@@ -53,6 +53,15 @@ var tag_data = [
     { id: 3, name: 'soup' }
 ];
 
+var ingredient_data = [
+    { id: 0, name: 'pumpkin' },
+    { id: 1, name: 'water' },
+    { id: 2, name: 'chicken' },
+    { id: 3, name: 'herb' },
+    { id: 4, name: 'bread' },
+    { id: 5, name: 'garlic' }
+];
+
 var recipe_data = [
     {
         id: 0,
@@ -73,26 +82,65 @@ var recipe_data = [
 
 var step_data = [
     [
-        { number: 1, ingredient: 'pumpkin', description: 'squash up some pumpkin', unit:'kg', amount:1 },
-        { number: 2, ingredient: 'water', description: 'add water', unit:'L', amount:0.5 }
+        {
+            number: 1,
+            ingredient: ingredient_data[0],
+            description: 'squash up some pumpkin',
+            unit:'kg',
+            amount:1
+        },
+        {
+            number: 2,
+            ingredient: ingredient_data[1],
+            description: 'add water',
+            unit:'L',
+            amount:0.5
+        }
     ],
     [
-        { number: 1, ingredient: 'chicken', description: 'cook a chicked', unit:'unit', amount:1 },
-        { number: 2, ingredient: 'herb', description: 'add herbs', unit:'g', amount:100 },
-        { number: 3, ingredient: 'bread', description: 'serve with bread', unit:'loaf', amount:1 }
+        {
+            number: 1,
+            ingredient: ingredient_data[2],
+            description: 'cook a chicked',
+            unit:'unit',
+            amount:1
+        },
+        {
+            number: 2,
+            ingredient: ingredient_data[3],
+            description: 'add herbs',
+            unit:'g',
+            amount:100
+        },
+        {
+            number: 3,
+            ingredient: ingredient_data[4],
+            description: 'serve with bread',
+            unit:'loaf',
+            amount:1
+        }
     ],
     [
-        { number: 1, ingredient: 'bread', description: 'get a bread', unit:'loaf', amount:1 },
-        { number: 2, ingredient: 'garlic', description: 'add garlic', unit:'clove', amount:5 },
-        { number: 3, ingredient: 'bread', description: 'get another bread', unit:'loaf', amount:1 }
+        {
+            number: 1,
+            ingredient: ingredient_data[4],
+            description: 'get a bread',
+            unit:'loaf',
+            amount:1
+        },
+        {
+            number: 2,
+            ingredient: ingredient_data[5],
+            description: 'add garlic',
+            unit:'clove',
+            amount:5
+        },
+        {
+            number: 3,
+            ingredient: ingredient_data[4],
+            description: 'another one',
+            unit:'loaf',
+            amount:1
+        }
     ]
-];
-
-var ingredient_data = [
-    { id: 0, name: 'pumpkin' },
-    { id: 1, name: 'water' },
-    { id: 2, name: 'chicken' },
-    { id: 3, name: 'herb' },
-    { id: 4, name: 'bread' },
-    { id: 5, name: 'garlic' }
 ];
