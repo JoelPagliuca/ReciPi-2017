ca7b862bc96019e6647042f834ce7a3160e637e8
Team-MC2 <teammc2monash@gmail.com>
2016-05-14 19:30:10 +1000

    added step editing feature

--- com.jpagliuca.recipi/src/main/web/recipe/edit-recipe.controller.js
+++ com.jpagliuca.recipi/src/main/web/recipe/edit-recipe.controller.js
@@ -14,14 +14,16 @@ editRecipeController.$inject = ['$scope', '$routeParams', 'recipeService'];
  */
 function editRecipeController ($scope, $routeParams, recipeService) {
     $scope.allTags = recipeService.getTags();
+    $scope.allIngredients = recipeService.getIngredients();
 
-    $scope.steps = {};
+    $scope.steps = [];
     $scope.tags = [];
     $scope.recipe = { tags: $scope.tags };
     $scope.recipe.name = "";
 
     $scope.step = {};
     $scope.tag = {};
+    $scope.step_number = 1;
 
     /**
      * TODO input validation
@@ -31,8 +33,13 @@ function editRecipeController ($scope, $routeParams, recipeService) {
         $scope.tag = {};
     };
 
+    /**
+     * TODO input validation
+     */
     this.addStep = function() {
+        $scope.step.number = $scope.step_number;
         $scope.steps.push($scope.step);
         $scope.step = {};
+        $scope.step_number += 1;
     };
 }
