4fbcda579d9c503c750095674e4b785cd812fc52
JoelPagliuca <joelpagliuca@hotmail.com>
2016-08-27 10:58:44 +1000

    added commenting for views

--- com.jpagliuca.recipi/src/main/api/recipiApi/recipi/views.py
+++ com.jpagliuca.recipi/src/main/api/recipiApi/recipi/views.py
@@ -1,23 +1,29 @@
+"""
+views
+
+Gives access to the Models
+"""
 from rest_framework import viewsets
 
 from models import *
 
 from serializers import *
 
+
 class IngredientViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows Ingredients to be viewed or edited
-    """
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
+
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
 
 class StepViewSet(viewsets.ModelViewSet):
     queryset = Step.objects.all()
