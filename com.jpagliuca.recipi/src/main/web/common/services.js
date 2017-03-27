2c53b35a3d8fde583e0859aadaefed0c545f8e1e
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2017-03-27 17:17:07 +1100

    updated the services.js to match the backend

--- com.jpagliuca.recipi/src/main/web/common/services.js
+++ com.jpagliuca.recipi/src/main/web/common/services.js
@@ -13,16 +13,43 @@ function recipeService($http, API) {
 
     var self = this;
 
+    /**
+     * get the steps for a recipe
+     * @param {*} recipe_id
+     */
     self.getSteps = function(recipe_id) {
         return $http.get(API+'/steps/'+recipe_id+'/');
     }
 
+    /**
+     * get all the recipes
+     * TODO make a /count endpoint
+     */
     self.getRecipes = function() {
         return $http.get(API+'/recipes/');
     }
 
+    /**
+     * get the full recipe, including steps
+     */
     self.getRecipe = function(id) {
         return $http.get(API+'/recipes/'+id+'/');
+    }
+
+    /**
+     * send a new recipe to the API
+     */
+    self.postRecipe = function(recipe) {
+        return $http.post(API+'/recipes/', recipe);
+    }
+
+    /**
+     * add a tag to a recipe
+     */
+    self.tagRecipe = function(recipe_id, tag_id) {
+        return $http.post(API+'/recipes/'+recipe_id+'/tag/', {
+            tag_id: tag_id
+        });
     }
 
     self.getIngredients = function() {
