2f673e35b74011387e14c76200c0aa25273a1cb6
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2017-04-29 13:51:11 +1000

    added the ingredient page basic files

---
+++ com.jpagliuca.recipi/src/main/web/ingredient/ingredient.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('recipi')
+
+    .controller('ingredientCtrl', ingredientCtrl);
+
+ingredientCtrl.$inject = ['$scope', 'recipeService'];
+
+/**
+ * CRUD ops for Ingredient
+ * @param $scope
+ * @param recipeService
+ */
+function ingredientCtrl ($scope, recipeService) {
+    
+}
---
+++ com.jpagliuca.recipi/src/main/web/ingredient/ingredient.service.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('recipi').service('ingredientService', ingredientService);
+
+ingredientService.$inject = ['$http', 'API'];
+
+/**
+ * interface with the Ingredient API
+ * @param {*} http does the http calls
+ * @param {*} API API URL
+ */
+function ingredientService($http, API) {
+
+    var self = this;
+
+    self.getIngredients = function() {
+        return $http.get(API+'/ingredients/');
+    }
+
+    self.getIngredient = function(id) {
+        return $http.get(API+'/ingredients/'+id+'/');
+    }
+}
