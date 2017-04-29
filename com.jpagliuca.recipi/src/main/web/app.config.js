d915c663fce0b541aa437d64138c13600308dc8e
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2017-04-29 13:50:45 +1000

    refactored out the routing config

--- com.jpagliuca.recipi/src/main/web/app.config.js
+++ com.jpagliuca.recipi/src/main/web/app.config.js
@@ -4,48 +4,6 @@
  */
 var app = angular.module('recipi');
 
-/**
- * Routing config
- */
-app.config(function($routeProvider) {
-    $routeProvider
-        // home page
-        .when('/home', {
-            templateUrl: 'home/home.html',
-            controller: homeController
-        })
-
-        // login page
-        .when('/login', {
-            templateUrl: 'login/login.html',
-            controller: loginController
-        })
-
-        // recipe view
-        .when('/recipe', {
-            templateUrl: 'recipe/recipe.html',
-            controller: 'recipeController'
-        })
-
-        // recipe edit view
-        .when('/recipe/edit', {
-            templateUrl: 'recipe/edit-recipe.html',
-            controller: 'editRecipeController',
-            controllerAs: 'editCtrl'
-        })
-
-        // recipe list view
-        .when('/recipes', {
-            templateUrl: 'recipeList/recipe-list.html',
-            controller: 'recipeListController'
-        })
-
-        .otherwise({
-            templateUrl: 'home/home.html',
-            controller: homeController
-        })
-});
-
 app.config(['$locationProvider', function($locationProvider) {
     $locationProvider.hashPrefix('');
 }]);
